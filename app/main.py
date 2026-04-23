99736b5a9a622632a1f0638dea254e48b15ab506
ballyees <shouldertip@gmail.com>
2026-04-24 03:39:28 +0700

    feat | add ping endpoint and middleware configuration for CORS

--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI
 from app.models.pricing_models import PriceRequest, PriceResponse
 from app.graph.graph import graph
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 
 app = FastAPI()
 
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
 
 @app.post("/price", response_model=PriceResponse)
 async def price(req: PriceRequest):
+    start = datetime.now()
     result = await graph.ainvoke({
         "product_name": req.product_name,
         "campaign": req.campaign_code
     })
+    end = datetime.now()
+    print("Total processing time:", (end - start).total_seconds(), "seconds")
 
     return result
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],              # List of allowed origins
+    allow_credentials=True,           # Allow cookies/authentication headers
+    allow_methods=["*"],              # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],              # Allow all headers
+)
