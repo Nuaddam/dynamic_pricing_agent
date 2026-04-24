c04d2859a81dd65c0c2a1a1a10d560670a3a4537
ballyees <shouldertip@gmail.com>
2026-04-24 11:22:52 +0700

    feat | implement generate_content function for content generation using Gemini API

---
+++ app/utils/gemini.py
@@ -0,0 +1,22 @@
+from google import genai
+from google.genai import types
+from typing import List, Optional
+
+
+def generate_content(
+    parts: List[types.Part], system_instruction: Optional[str] = None, response_schema: Optional[types.SchemaUnion] = None
+) -> types.GenerateContentResponse:
+    client = genai.Client()
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=types.Content(
+            parts=parts,
+            role="user",
+        ),
+        config=types.GenerateContentConfig(
+            system_instruction=system_instruction,
+            response_schema=response_schema,
+            response_mime_type="application/json" if response_schema else None,
+        ),
+    )
+    return response
